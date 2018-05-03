dc29b531e0eabdfa28d7d90c20c1e85718c9da3f
unknown <jernej.banevec@gmail.com>
2018-05-03 14:18:22 +0200

    Dodana Excel datoteka z zdravili in sql stavki - zadnji stavek ne deluje

---
+++ tabele.sql
@@ -0,0 +1,71 @@
+DROP TABLE IF EXISTS zdravnik CASCADE
+DROP TABLE IF EXISTS oseba CASCADE
+DROP TABLE IF EXISTS pregled CASCADE
+DROP TABLE IF EXISTS test CASCADE
+DROP TABLE IF EXISTS specializacija CASCADE
+DROP TABLE IF EXISTS diagnoza CASCADE
+DROP TABLE IF EXISTS bolezen CASCADE
+DROP TABLE IF EXISTS zdravila CASCADE
+
+CREATE TABLE bolezen (
+	bolezenID INTEGER PRIMARY KEY,
+	ime TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE zdravila (
+	zdravilaID INTEGER PRIMARY KEY,
+	ime TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE test (
+	testID INTEGER PRIMARY KEY,
+	ime TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE zdravnik (
+	zdravnikID INTEGER PRIMARY KEY,
+	ime TEXT NOT NULL,
+	priimek TEXT NOT NULL
+);
+
+CREATE TABLE oseba (
+	osebaID INTEGER PRIMARY KEY,
+	ime TEXT NOT NULL,
+	priimek TEXT NOT NULL,
+	kri TEXT NOT NULL,
+	teza DECIMAL NOT NULL,
+	visina DECIMAL NOT NULL,
+	osebniZdravnik INTEGER NOT NULL REFERENCES zdravnik(zdravnikID)
+);
+
+CREATE TABLE specializacija (
+    zdravnik INTEGER NOT NULL REFERENCES zdravnik(zdravnikID) ON DELETE CASCADE,
+    test INTEGER NOT NULL REFERENCES test(testID) ON DELETE CASCADE,
+    PRIMARY KEY (zdravnik, test)
+);
+
+CREATE TABLE diagnoza (
+	diagnozaID INTEGER PRIMARY KEY,
+	bolezen INTEGER NOT NULL REFERENCES bolezen(bolezenID),
+	zdravilo INTEGER NOT NULL REFERENCES zdravilo(zdraviloID),
+	zdravnik INTEGER NOT NULL REFERENCES zdravnik(zdravnikID)
+);
+
+CREATE TABLE pregled (
+    pregledID SERIAL PRIMARY KEY,
+	oseba INTEGER NOT NULL REFERENCES oseba(osebaID),
+	zdravnik INTEGER NOT NULL REFERENCES zdravnik(zdravnikID),
+	testZdaj INTEGER NOT NULL REFERENCES test(testID),
+	testNaprej INTEGER NOT NULL REFERENCES test(testID),
+	diagnoza INTEGER REFERENCES diagnoza(diagnozaID),
+	izvid TEXT,
+	datum DATE DEFAULT now() CHECK (datum <= now())
+);
+
+/* ne deluje */
+CREATE TRIGGER posodobitev AFTER INSERT ON pregled
+FOR EACH ROW
+WHEN (last(diagnoza) <> NULL),
+   UPDATE pregled
+   SET pregled(diagnoza) = last(pregled(diagnoza))
+   WHERE pregled(oseba) = last(pregled(oseba)) AND pregled(diagnoza) = NULL;
