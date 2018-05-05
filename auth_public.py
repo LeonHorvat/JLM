12be57d13d0eb6454081120e409d008ea3656f35
Metod Jazbec <jazbec.metod@gmail.com>
2018-05-05 16:48:27 +0200

    dodan login

---
+++ auth_public.py
@@ -0,0 +1,4 @@
+db = 'sem2018_metodj'
+host = 'baza.fmf.uni-lj.si'
+user = 'metodj'
+password = 'gilbertarenas0'
--- meso.py
+++ meso.py
@@ -1,8 +1,39 @@
 from bottle import *
+import auth_public as auth
+import psycopg2, psycopg2.extensions, psycopg2.extras
+import hashlib
+
+################
+#priklop na bazo
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s sumniki
+baza = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+baza.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogocimo transakcije
+cur = baza.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+################
+#test priklopa na bazo
+def test(posta):
+    cur.execute('''
+                    SELECT * FROM uporabnik
+
+                ''', [posta])
+    return (cur.fetchall())
+
+print(test('posta'))
 
 
+################
+#bottle uvod, pomozne funkcije
 static_dir = "./static"
 
+def password_md5(s):
+    """Vrni MD5 hash danega UTF-8 niza. Gesla vedno spravimo v bazo
+       kodirana s to funkcijo."""
+    h = hashlib.md5()
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
+
+print(password_md5('mat555'))
 
 @route("/static/<filename:path>")
 def static(filename):
@@ -10,22 +41,42 @@ def static(filename):
        /static/..."""
     return static_file(filename, root=static_dir)
 
+
+################
+#bottle routes
 @get("/login/")
 def login_get():
     """Serviraj formo za login."""
     #curuser = get_user(auto_redir = True)
     return template("login.html",
                            napaka=None,
-                           logged=None,
                            username=None)
+
+@post("/login/")
+def login_post():
+    """Obdelaj izpolnjeno formo za prijavo"""
+    # Uporabnisko ime, ki ga je uporabnik vpisal v formo
+    username = request.forms.username
+    # Izracunamo MD5 has gesla, ki ga bomo spravili
+    password = password_md5(request.forms.password)
+    # Preverimo, ali se je uporabnik pravilno prijavil
+    c = baza.cursor()
+    c.execute("SELECT 1 FROM uporabnik WHERE username=%s AND password=%s",
+              [username, password])
+    if c.fetchone() is None:
+        # Username in geslo se ne ujemata
+        return template("login.html",
+                               napaka="Nepravilna prijava",
+                               username=username)
+    else:
+        # Vse je v redu, nastavimo cookie in preusmerimo na glavno stran
+        #response.set_cookie('username', username, path='/', secret=secret)
+        redirect("/index/")
 
 @get("/index/")
 def index():
     """Serviraj formo za login."""
     #curuser = get_user(auto_redir = True)
-    return template("index.html",
-                           napaka=None,
-                           logged=None,
-                           username=None)
+    return template("index.html")
 
 run(host='localhost', port=8080)
