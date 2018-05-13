06571ac2903fe01e4845742373325f50c7a7f4df
LeonHorvat <leon.horvat1@gmail.com>
2018-05-13 16:43:33 +0200

    spremembe funkcije kartoteka

--- meso.py
+++ meso.py
@@ -143,7 +143,7 @@ def index():
     elif pooblastilo(curuser[0]) == 'direktor':
         redirect('/indexdirektor/')
     else:
-        return template("index.html", user=curuser[0], click = False)
+        return template("index.html", user=curuser[0], click = False, napaka = None)
 
 @post("/index/")
 def kartoteka():
@@ -151,20 +151,42 @@ def kartoteka():
     ID = request.forms.ID
     curuser = get_user()
     c = baza.cursor()
-    c.execute("""SELECT DISTINCT pregled.datum, bolezen.ime  FROM pregled
-                JOIN oseba ON pregled.oseba = oseba.osebaID
-                JOIN diagnoza
-                JOIN bolezen ON diagnoza.bolezen = bolezen.bolezenID
-                ON pregled.diagnoza = diagnoza.diagnozaID
-                WHERE oseba.osebaID = %s
-                ORDER BY pregled.datum DESC""",
-              [ID])
+    if ID is not None:
+        c.execute("""SELECT DISTINCT pregled.datum, bolezen.ime  FROM pregled
+                    JOIN oseba ON pregled.oseba = oseba.osebaID
+                    JOIN diagnoza
+                    JOIN bolezen ON diagnoza.bolezen = bolezen.bolezenID
+                    ON pregled.diagnoza = diagnoza.diagnozaID
+                    WHERE oseba.osebaID = %s
+                    ORDER BY pregled.datum DESC""",
+                  [ID])
+
+
+    else:
+        #iz vpisanega imena, priimka in datuma rojstva vrni tabelo diagnoz te osebe, razvrščene po datumu
+        ime = request.forms.ime
+        priimek = request.forms.priimek
+        rojstvo = request.forms.datum
+        c.execute("""SELECT DISTINCT pregled.datum, bolezen.ime  FROM pregled
+                    JOIN oseba ON pregled.oseba = oseba.osebaID
+                    JOIN diagnoza
+                    JOIN bolezen ON diagnoza.bolezen = bolezen.bolezenID
+                    ON pregled.diagnoza = diagnoza.diagnozaID
+                    WHERE oseba.ime = %s AND oseba.priimek = %s AND oseba.rojstvo = %s
+                    ORDER BY pregled.datum DESC""",
+                  [ime, priimek, rojstvo])
+
     tmp = c.fetchall()
-    if tmp is None:
+    d = baza.cursor()
+    d.execute("""SELECT oseba.ime, oseba.priimek FROM oseba
+                WHERE oseba.osebaID = %s""",
+              [ID])
+    ime_priimek = d.fetchone()
+    if len(tmp) == 0:
         # ID osebe v bazi ne obstaja
-        return template("index.html", napaka="Nepravilna poizvedba")
-    if tmp is not None:
-        return template("index.html", rows=tmp, click = True, napaka = None, user=curuser[0])
+        return template("index.html", napaka="Nepravilna poizvedba, ID ne obstaja", user=curuser[0], click = False)
+    else:
+        return template("index.html", rows=tmp, ime_priimek = ime_priimek, click = True, napaka = None, user=curuser[0])
 
 
 
